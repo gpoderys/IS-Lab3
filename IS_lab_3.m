f84de18598f9e2265d759c5611c4a4a4ec46c5ae
Gintautas Poderys <gintautasp25@gmail.com>
2021-12-14 01:35:34 +0200

    Aproksimatorius patikrintas

--- IS_lab_3.m
+++ IS_lab_3.m
@@ -7,14 +7,14 @@ x =[0:1/22:1];
 d = ((1 + 0.6*sin(2*pi*x/0.7))+(0.3*sin(2*pi*x)))/2;
 figure(1), plot(x,d), grid on;
 
-c1 = 0.2;
-c2 = 0.9;
-r1 = 0.1;
-r2 = 0.1;
+c1 = 0.18;
+c2 = 0.92;
+r1 = 0.25;
+r2 = 0.21;
 
-w1 = rand(1)
-w2 = rand(1)
-b = rand(1)
+w1 = rand(1);
+w2 = rand(1);
+b = rand(1);
 lr= 0.1;
 
 %Aproksimatoriaus apmokymas
@@ -30,3 +30,15 @@ for i = 1:10000
         b = b + lr*e;
     end
 end
+
+%Aproksimatoriaus testavimas
+X = [0:1/22:1];
+
+Y = zeros(1, length(X));
+for index=1:length(X)
+    F1=exp(-(((X(index)-c1)^2)/(2*r1^2)));
+    F2=exp(-(((X(index)-c2)^2)/(2*r2^2)));
+    y=F1*w1+F2*w2+b;
+    Y(index)=y;
+end
+hold on , plot(X,Y,'r+'), hold off
